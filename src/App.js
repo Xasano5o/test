739d91d0bc921c6230a69c299f0aadc3c713efef
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-17 19:48:01 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Navbar from './Components/Navbar';
+import Navbar1 from './Components/Navbar-1';
+import Navbar2 from './Components/Navbar-2';
+import Navbar3 from './Components/Navbar-3';
 
 export const App = () => {
     return(
-        <div>
-            <Navbar />
-         
+        <div style={{ background: '#4188ab', width: '100%',height: '100vh', paddingTop: '30px' }}>
+            <Navbar1 />
+            <Navbar2 />
+            <Navbar3 />
 
         </div>
     )
