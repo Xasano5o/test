2ce3457db45e39cb626cb5adbaddbf0c9007de79
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-18 18:39:02 +0500

    done

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ root.render(
 
 
             //   1 -dars react kirish 
-
 // React nega yaxwiroq ===>    wo`rchivel doom ishladsak . tasir qiladi butin prekt yurg`izmaydi
 // React nima ==>        faceboook tomonidan ishlab chiqilgan bulib, javascript kutubxonasi    
 // Unic derition data folor ==>    malumotlar birtomonga oqadi pastga qarab agar 
@@ -26,4 +25,11 @@ root.render(
 // search injen optimatation ===>   internetda qidirganda birinchi ma`lumot chiqshi react yozilgan code tezroq chiqadi, server sigin rendering bolsa prekt tezro ishlaydi
 // JSX ==>  React yozilgan code jsx formadda bo`ladi, Java script Exmel birlashmasi [Exmel]=> html oldingi verselari  , React creat elemnt shugasi, htmldagi code ichida java script ishladsa bo`ladi, java script code ihida htmlga ishladsa bo`ladi
 
+
+
+//           2 -dars 
+//  render ==> brazerga malumot chiqarib beradi 
+// import ==> ma`lumotni qabul qivolish 
+// export defalt vs export ==> export defalt  importda istalgan nom bilan tutib olishimiz mumkin va 1 martotaba bitta jsx file ichida ishlata olamiz  
+// 
 
