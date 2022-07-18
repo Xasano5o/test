adf41c0b18747d645c02d2f74ca9b424dab9c6c0
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-18 17:07:04 +0500

    done

--- src/index.js
+++ src/index.js
@@ -11,5 +11,19 @@ root.render(
 );
 
 
+            //   1 -dars react kirish 
+
+// React nega yaxwiroq ===>    wo`rchivel doom bor shunga i malumotlarni qidirganda react yozilgan bulsa tezroq chiqadi 
+// React nima ==>        faceboook tomonidan ishlab chiqilgan bulib, javascript kutubxonasi    
+// Unic derition data folor ==>    malumotlar birtomonga oqadi pastga qarab agar 
+// child malumot bittasida ma`lumot ishlamay qolsa parent otasiga tasir qilmaydi 
+// Be diraction data folor ==>      xammasiga tasir qiladi bitta xatolik bersa otasiga ham tasir qiladi ma`lumotlar
+// npx create - react - app ==>    react file ochvolish un kalit keyi cmd yozamiz 
+// npm statrt    ===> prect yurg`izish 
+// git ignor ==> githubga push qilinmaydigan ma`lumotlarni o`zida saqlaydi 
+// git clone ==>   githubdan tortib olingan file uzimiz notebook versiyasiga moslashtirvolish githubdan react file bilan tortganimzda node models b`olmaydi 
+// npm i || inistal ==>  git hubdan tortilgan react filega yurg`izganmizdan node modeli bo`lmaydi node model o`rnatish uchun uzimizni notebookmizga mos versesiyasi o`rnatish uchun 
+// search injen optimatation ===>   internetda qidirganda birinchi ma`lumot chiqshi react yozilgan code tezroq chiqadi, server siden rendering boqlsa prekt tezro ishlaydi
+// JSX ==>  React yozilgan code jsx formadda bo`ladi, Java script Exmel birlashmasi [Exmel]=> html oldingi verselari  , React creat elemnt shugasi, htmldagi code ichida java script ishladsa bo`ladi, java script code ihida htmlga ishladsa bo`ladi
 
 
