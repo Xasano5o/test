a97d8df7ffdc049c026dd242155b9a0a433c440b
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-19 22:35:21 +0500

    done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import Navbar1 from './Components/Navbar-1';
 import Navbar2 from './Components/Navbar-2';
 import Navbar3 from './Components/Navbar-3';
+import './index.css'
+import AngularIcon from './Assets/angular.png'
+import Image from './Assets/img.png'
+
+
+import One_prect from './comment/one.jsx'
 
 export const App = () => {
-    return(
-        <div style={{ background: '#4188ab', width: '100%',height: '100vh', paddingTop: '30px' }}>
-            <Navbar1 />
+    return (
+        <div>
+            {/* /* <Navbar1 />
             <Navbar2 />
-            <Navbar3 />
-
+            <Navbar3 /> */ }
+            <div className='box'>
+                <div className='menu_1'><One_prect amg={Image} /></div>
+                <div className='menu_2'><One_prect amg={AngularIcon} /></div>
+            </div>
         </div>
     )
 }
 
-//fdslkhjflsdjflidsjflds
 
 export default App;
